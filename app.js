9d25ba2f38c4e3ccaa0590eb8417f3b8ad756463
Leechiayu <gary86442@gmail.com>
2023-02-23 17:10:16 +0800

    feat:porject init

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const routes = require("./routes");
+const app = express();
+
+const port = 3000;
+
+// setting template engine
+app.engine("hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
+app.use(routes);
+
+// server listening
+app.listen(port, () => {
+  console.log(`listening on http://localhost:${port}`);
+});
