46ec12c5bff2b56915e97f6cfdc037fb4dd762d0
Leechiayu <gary86442@gmail.com>
2023-02-24 12:21:26 +0800

    feat:craate new shorten then render show

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -8,43 +8,35 @@ router.get("/", (req, res) => {
 //建立短網址
 router.post("/", (req, res) => {
   const origin = req.body.url;
-
-  const shorten = require("../../public/javascripts/gibberish_generator");
-  LinksDB.create({ origin, shorten }).then(() => {
-    res.render("show", { shorten });
-  });
   // 判斷是否有建立過短網址
-  // LinksDB.find({ origin })
-  //   .lean()
-  //   .then((links) => {
-  //     console.log(links);
-  //     // 如果有 直接輸出舊有的資料
-  //     // if (links.length) {
-  //     //   res.render("/show", { links });
-  //     // } else {
-  //     //   // 沒建過，生成短網址(要判斷生成出的短網址是否重複)
-  //     //   let IsShortenDuplicated = true;
-  //     //   while (IsShortenDuplicated) {
-  //     //     const shorten = require("../../public/javascripts/gibberish_generator");
-  //     //     console.log(shorten);
-  //     //     LinksDB.findOne(
-  //     //       { shorten }.then((links) => {
-  //     //         console.log(`sec.${links}`);
-  //     //         //如果短網址找不到資料則新增資料。
-  //     //         if (!links.length) {
-  //     //           IsShortenDuplicated = false;
-  //     //           LinksDB.create({ origin, shorten }).then(() => {
-  //     //             LinksDB.findOne({ origin })
-  //     //               .lean()
-  //     //               .then((links) => {
-  //     //                 res.render("show", { links });
-  //     //               });
-  //     //           });
-  //     //         }
-  //     //       })
-  //     //     );
-  //     //   }
-  //     // }
-  //   });
+  LinksDB.find({ origin })
+    .lean()
+    .then((links) => {
+      // 如果有 直接輸出舊有的資料
+      if (links.length) {
+        res.render("show", { links });
+      } else {
+        // 沒建過，生成短網址(要判斷生成出的短網址是否重複)  因非同步問題先跳過功能
+        // let IsShortenDuplicated = true;
+        // while (IsShortenDuplicated) {
+        const shorten = require("../../public/javascripts/gibberish_generator");
+        // console.log(shorten);
+        LinksDB.findOne({ shorten }).then((links) => {
+          //如果短網址找不到資料則新增資料。
+          if (!links) {
+            // IsShortenDuplicated = false;
+            LinksDB.create({ origin, shorten }).then(() => {
+              LinksDB.findOne({ origin })
+                .lean()
+                .then((links) => {
+                  res.render("show", { links });
+                });
+            });
+          }
+        });
+      }
+    });
+  // }
 });
+
 module.exports = router;
