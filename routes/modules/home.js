b6c4fc964bfcd2c9d4ee4ef29bcac99e0b779335
Leechiayu <gary86442@gmail.com>
2023-02-27 21:47:32 +0800

    fix:adjust brackets

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -17,23 +17,24 @@ router.post("/", (req, res) => {
         res.render("index", { links });
       } else {
         // 沒建過，生成短網址(要判斷生成出的短網址是否重複)  因非同步問題先跳過功能
-        // let IsShortenDuplicated = true;
-        // while (IsShortenDuplicated) {
-        const shorten = require("../../public/javascripts/gibberish_generator");
-        // console.log(shorten);
-        LinksDB.findOne({ shorten }).then((links) => {
-          //如果短網址找不到資料則新增資料。
-          if (!links) {
-            // IsShortenDuplicated = false;
-            LinksDB.create({ origin, shorten }).then(() => {
-              LinksDB.findOne({ origin })
-                .lean()
-                .then((links) => {
-                  res.render("index", { links });
-                });
-            });
-          }
-        });
+        let IsShortenDuplicated = true;
+        while (IsShortenDuplicated) {
+          const shorten = require("../../public/javascripts/gibberish_generator");
+          // console.log(shorten);
+          LinksDB.findOne({ shorten }).then((links) => {
+            //如果短網址找不到資料則新增資料。
+            if (!links) {
+              IsShortenDuplicated = false;
+              LinksDB.create({ origin, shorten }).then(() => {
+                LinksDB.findOne({ origin })
+                  .lean()
+                  .then((links) => {
+                    res.render("index", { links });
+                  });
+              });
+            }
+          });
+        }
       }
     });
   // }
