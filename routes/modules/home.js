d1433242304a8cb4753fd4f1334cb049ce047e35
Leechiayu <gary86442@gmail.com>
2023-02-27 16:13:01 +0800

    feat:add shorten route & show page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -9,11 +9,11 @@ router.get("/", (req, res) => {
 router.post("/", (req, res) => {
   const origin = req.body.url;
   // 判斷是否有建立過短網址
-  LinksDB.find({ origin })
+  LinksDB.findOne({ origin })
     .lean()
     .then((links) => {
       // 如果有 直接輸出舊有的資料
-      if (links.length) {
+      if (links) {
         res.render("show", { links });
       } else {
         // 沒建過，生成短網址(要判斷生成出的短網址是否重複)  因非同步問題先跳過功能
@@ -39,4 +39,13 @@ router.post("/", (req, res) => {
   // }
 });
 
+//處理短網址導向
+router.get("/:shorten", (req, res) => {
+  const shorten = req.params.shorten;
+  if (shorten !== "favicon.ico") {
+    LinksDB.findOne({ shorten }).then((links) => {
+      res.redirect(`${links.origin}`);
+    });
+  }
+});
 module.exports = router;
